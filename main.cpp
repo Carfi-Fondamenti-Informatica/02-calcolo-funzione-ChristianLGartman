160e59f8321ca5fd8cf3daebb5b115a97a8407f6
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-11-15 12:04:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 using namespace std;
 
 int main() {
-   return 0;
+  float a, b, x, y, z;
+  cout<<"dimmi i valori di a"<< endl;
+  cin>> a;
+  cout<<"dimmi i valori di b"<< endl;
+  cin>> b;
+  cout<<"dimmi i valori di x"<< endl;
+  cin>> x;
+  cout<<"dimmi i valori di y"<< endl;
+  cin>> y;
+  if (x<0&&y>0) {
+      z=(a*x)-(b*y);
+      cout<<z;
+  }
+  else if(x>=0&&y<=0){
+      z=(a*x*x)-(b*y);
+      cout<<z;
+  }
+  else {
+      z=(a*x)+(b*y*y);
+      cout<<z;
+  }
+  return 0;
 }
